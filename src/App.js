b98601c593a0498a48e5f3c493fffc05c8842589
Sophon-Kr <77550438+Sophon-Kr@users.noreply.github.com>
2022-03-08 14:52:35 +0700

    add main page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 // import logo from "./logo.svg";
 import "./App.css";
-import Homepage from "./pages/Homepage";
+import AboutMe from "./pages/AboutMe";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import configureStore from "./middleware/store";
-
 import { Provider } from "react-redux";
-// import configureStore from "./middleware/store";
-// import * as actions from "../src/middleware/action";
+import Education from "./pages/Education";
+import Skills from "./pages/Skills";
+import Experiences from "./pages/Experiences";
 
 function App() {
   const store = configureStore();
@@ -14,9 +14,11 @@ function App() {
     <Provider store={store}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Homepage />} />
-          {/* <Route path="expenses" element={<Expenses />} />
-      <Route path="invoices" element={<Invoices />} /> */}
+          <Route exact path="education" element={<Education />} />
+          <Route exact path="skills" element={<Skills />} />
+          <Route exact path="experiences" element={<Experiences />} />
+          <Route exact path="/" element={<AboutMe />} />
+          <Route element={<AboutMe />} />
         </Routes>
       </BrowserRouter>
     </Provider>
