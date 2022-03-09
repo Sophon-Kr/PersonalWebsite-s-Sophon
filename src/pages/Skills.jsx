5bdc7c9b7dfa7837bfde8a5c4ca0df2e687dbe5e
Sophon-Kr <77550438+Sophon-Kr@users.noreply.github.com>
2022-03-10 04:43:10 +0700

    edit detail in skills page

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,16 +1,37 @@
 import React from "react";
 import { connect } from "react-redux";
 import "../assets/skills.css";
+import { SiAdobephotoshop } from "react-icons/si";
+import { FaJava, FaDatabase } from "react-icons/fa";
+import { RiComputerFill, RiCodeBoxFill } from "react-icons/ri";
+
 const otherSkills = [
   {
     id: 1,
-    text: "Java",
-    icon: "Java",
+    text: (
+      <span className="center-text">
+        <div>Java</div>
+      </span>
+    ),
+    icon: (
+      <div className="center-text">
+        <FaJava className="size-icon"></FaJava>
+      </div>
+    ),
   },
   {
     id: 2,
-    text: "Adobe Photoshop",
-    icon: "Adobe Photoshop",
+    text: (
+      <span className="center-text">
+        <div>Adobe</div>
+        <div>Photoshop</div>
+      </span>
+    ),
+    icon: (
+      <div className="center-text">
+        <SiAdobephotoshop className="size-icon"></SiAdobephotoshop>
+      </div>
+    ),
   },
 ];
 
@@ -18,16 +39,50 @@ export const Skills = (props) => {
   return (
     <div className="skills" id="skills">
       <div className="maincontainer-skills">
-        <div className="header-skills">Web Skills </div>
+        <div className="header-skills-1">Web Skills </div>
         <div className="container-skills-web">
-          <div className="card-skills">1</div>
-          <div className="card-skills">2</div>
-          <div className="card-skills">3</div>
+          <div className="card-skills">
+            <div className="header-text-skills">
+              <FaDatabase></FaDatabase>
+              <span
+                style={{
+                  marginLeft: "15px",
+                }}
+              >
+                Database
+              </span>
+            </div>
+          </div>
+          <div className="card-skills">
+            <div className="header-text-skills">
+              <RiComputerFill></RiComputerFill>
+              <span
+                style={{
+                  marginLeft: "15px",
+                }}
+              >
+                Front-End
+              </span>
+            </div>
+          </div>
+          <div className="card-skills">
+            <div className="header-text-skills">
+              <RiCodeBoxFill></RiCodeBoxFill>
+              <span
+                style={{
+                  marginLeft: "15px",
+                }}
+              >
+                Back-End
+              </span>
+            </div>
+          </div>
         </div>
-        <div className="header-skills">Program and Other Skills </div>
+        <div className="header-skills-2">Programing and Other Skills </div>
         <div className="container-skills-other">
           {otherSkills.map((data) => (
             <div className="item-skills" key={data.id}>
+              {data.icon}
               {data.text}
             </div>
           ))}
