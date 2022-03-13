3255b237fa869e8687d17b7cc7593ed29ad125c0
Sophon-Kr <77550438+Sophon-Kr@users.noreply.github.com>
2022-03-14 01:57:00 +0700

    add activity list

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -11,37 +11,85 @@ export const Activity = (props) => {
           <ul>
             <li>
               <div className="timeline-content">
-                <h1>Header 1</h1>
+                <h1>Click Camp 12 (2020), Being a Staff</h1>
                 <p>
-                  The behavior could be thought of as a minimum gutter, as if
-                  the gutter is bigger somehow (because of something like
+                  • Organized the camp providing informing to 80 high-school
+                  students who are interested in Computer Engineering - Planed
+                  and managed some activities in the camp.
+                </p>
+                <p>• Planed and managed some activities in the camp.</p>
+              </div>
+            </li>
+            <li>
+              <div className="timeline-content">
+                <h1>
+                  Mahidol University's Choir (MU Choir) (2020) Being Vice
+                  President and Secretary of MU Choir.
+                </h1>
+                <p>
+                  • Prepared and submitted project documents to the relevant
+                  departments of the university.
                 </p>
               </div>
             </li>
             <li>
               <div className="timeline-content">
-                <h1>Header 2</h1>
+                <h1>Participant in the international contest (2019)</h1>
+                <p>• The 8th BICF - Bali International Choir Festival</p>
                 <p>
-                  The behavior could be thought of as a minimum gutter, as if
-                  the gutter is bigger somehow (because of something like
+                  • 12th International Choral Festival Orientale Concentus at
+                  Singapore
                 </p>
               </div>
             </li>
             <li>
               <div className="timeline-content">
-                <h1>Header 3</h1>
+                <h1>INCRISIS (2019), Being a Staff </h1>
                 <p>
-                  The behavior could be thought of as a minimum gutter, as if
-                  the gutter is bigger somehow (because of something like
+                  • Managed and sold food in marketing department activities by
+                  Computer Engineering student
                 </p>
               </div>
             </li>
             <li>
               <div className="timeline-content">
-                <h1>Header 4</h1>
+                <h1>
+                  Electronic Camp (2018), Being a Staff (Cooperated with
+                  electrical engineering student)
+                </h1>
+                <p>• Did a variety of activities in the camp.</p>
+              </div>
+            </li>
+            <li>
+              <div className="timeline-content">
+                <h1>
+                  WE NEED YOU SMO61 Project (2018) Being a Coordination
+                  Department
+                </h1>
                 <p>
-                  The behavior could be thought of as a minimum gutter, as if
-                  the gutter is bigger somehow (because of something like
+                  • Learned about the work process and supported in many
+                  projects in Mahidol University Student Association
+                </p>
+              </div>
+            </li>
+            <li>
+              <div className="timeline-content">
+                <h1>
+                  Festival of Lights (2018) Being a head of sponsor coordination
+                </h1>
+                <p>
+                  • Contacted, coordinated, and managed the sponsor's part in
+                  the Festival of Lights 2018 by Mahidol University Student
+                  Association Mahidol University
+                </p>
+              </div>
+            </li>
+            <li>
+              <div className="timeline-content">
+                <h1>EG Open House (2018), Being a Volunteer student</h1>
+                <p>
+                  • Introduced about Computer Engineering Mahidol University and
+                  took high school students to visit the Digital lab
                 </p>
               </div>
             </li>
