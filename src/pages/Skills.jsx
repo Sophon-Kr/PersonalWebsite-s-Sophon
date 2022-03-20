faf1ab5cdb89a76c6b15e0fb4d0969d4daceaa3e
Sophon-Kr <77550438+Sophon-Kr@users.noreply.github.com>
2022-03-20 21:42:53 +0700

    edit webskill part

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -9,9 +9,19 @@ import {
   SiCplusplus,
   SiC,
   SiMicrosoftoffice,
+  SiPostgresql,
+  SiMongodb,
+  SiJavascript,
+  SiMaterialui,
+  SiBootstrap,
+  SiHtml5,
+  SiCss3,
+  SiNodedotjs,
 } from "react-icons/si";
-import { FaJava, FaDatabase } from "react-icons/fa";
+import { FaJava, FaReact, FaVuejs } from "react-icons/fa";
 import { RiComputerFill, RiCodeBoxFill } from "react-icons/ri";
+import { ImDatabase } from "react-icons/im";
+// import { SiPostgresql } from "react-icons/im";
 
 const otherSkills = [
   {
@@ -129,42 +139,102 @@ export const Skills = (props) => {
         <div className="header-skills-1">Web Skills </div>
         <div className="container-skills-web">
           <div className="card-skills">
-            <div className="header-text-skills">
-              <FaDatabase></FaDatabase>
-              <span
-                style={{
-                  marginLeft: "15px",
-                }}
-              >
-                Database PostgreSQL, MongoDB SQL,
-              </span>
+            <div
+              className="header-text-skills"
+              style={{ marginBottom: "20px" }}
+            >
+              <ImDatabase
+                style={{ width: "70px", height: "auto" }}
+              ></ImDatabase>
+              <div>Database</div>
+            </div>
+            <div className="skill-container-list">
+              <div className="skill-item-list">
+                <SiPostgresql
+                  style={{ width: "40px", height: "auto", color: "#336791" }}
+                ></SiPostgresql>
+                PostgreSQL{" "}
+              </div>
+              <div className="skill-item-list">
+                <SiMongodb
+                  style={{ width: "40px", height: "auto", color: "#62A354" }}
+                ></SiMongodb>
+                MongoDB
+              </div>
             </div>
           </div>
           <div className="card-skills">
             <div className="header-text-skills">
-              <RiComputerFill></RiComputerFill>
-              <span
-                style={{
-                  marginLeft: "15px",
-                }}
-              >
-                Front-End JavaScript React.js Vue.js Material-UI,
-                Bootstrap,HTML, CSS,
-              </span>
+              <RiComputerFill
+                style={{ width: "80px", height: "auto" }}
+              ></RiComputerFill>
+              <div>Front-End</div>
+            </div>
+            <div className="skill-container-frontend-list">
+              <div className="skill-container-list align-Self-center">
+                <div className="skill-item-list">
+                  <SiJavascript
+                    style={{ width: "25px", height: "auto", color: "#EFD81D" }}
+                  ></SiJavascript>
+                  JavaScript
+                </div>
+                <div className="skill-item-list">
+                  <FaReact
+                    style={{ width: "30px", height: "auto", color: "#5ED3F3" }}
+                  ></FaReact>
+                  React.js
+                </div>
+              </div>
+              <div className="skill-container-list align-Self-center">
+                <div className="skill-item-list">
+                  <FaVuejs
+                    style={{ width: "30px", height: "auto", color: "#3FB27F" }}
+                  ></FaVuejs>
+                  Vue.js
+                </div>
+                <div className="skill-item-list ">
+                  <SiHtml5
+                    style={{ width: "30px", height: "auto", color: "#DD4B25" }}
+                  ></SiHtml5>
+                  HTML
+                </div>
+              </div>
+              <div className="skill-container-list align-Self-center">
+                <div className="skill-item-list">
+                  <SiCss3
+                    style={{ width: "30px", height: "auto", color: "#006CB4" }}
+                  ></SiCss3>
+                  CSS
+                </div>
+                <div
+                  className="skill-item-list"
+                  style={{ textAlign: "center" }}
+                >
+                  <SiMaterialui
+                    style={{ width: "30px", height: "auto", color: "#007DC5" }}
+                  ></SiMaterialui>{" "}
+                  Material-UI
+                </div>
+              </div>
             </div>
           </div>
           <div className="card-skills">
-            <div className="header-text-skills">
-              <RiCodeBoxFill></RiCodeBoxFill>
-              <span
-                style={{
-                  marginLeft: "15px",
-                }}
-              >
-                Back-End
-                <br />
+            <div
+              className="header-text-skills"
+              style={{ marginBottom: "20px" }}
+            >
+              <RiCodeBoxFill
+                style={{ width: "80px", height: "auto" }}
+              ></RiCodeBoxFill>
+              <div>Back-End</div>
+            </div>
+            <div className="align-Self-center">
+              <div className="skill-item-list">
+                <SiNodedotjs
+                  style={{ width: "40px", height: "auto", color: "#70B556" }}
+                ></SiNodedotjs>
                 Node.js
-              </span>
+              </div>
             </div>
           </div>
         </div>
