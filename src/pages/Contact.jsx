1a211458635b032615e9e77886df0cf5c96f5ffd
Sophon-Kr <77550438+Sophon-Kr@users.noreply.github.com>
2022-03-31 20:06:45 +0700

    edit link resume

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -137,7 +137,7 @@ export const Contact = (props) => {
             </div>
             <a
               style={{ textDecoration: "none", color: "black" }}
-              href="https://drive.google.com/file/d/1RrONgt4uqxx8eE5H1rhMxF0X_FKooBXD/view?usp=sharing"
+              href="https://drive.google.com/file/d/1sGJFaj3xibk-hWMn8sJ8AoMwoXp8fedg/view?usp=sharing"
               target="_blank"
               rel="noreferrer"
             >
@@ -145,7 +145,7 @@ export const Contact = (props) => {
             </a>
             <a
               style={{ textDecoration: "none", color: "black" }}
-              href="https://drive.google.com/file/d/1PUvXPgmyVH26dYm8Labo9E5ttbZYSrTW/view?usp=sharing"
+              href="https://drive.google.com/file/d/1sI0HgKn1Q8tjX8avRgiPTeORgcRytyqI/view?usp=sharing"
               target="_blank"
               rel="noreferrer"
             >
